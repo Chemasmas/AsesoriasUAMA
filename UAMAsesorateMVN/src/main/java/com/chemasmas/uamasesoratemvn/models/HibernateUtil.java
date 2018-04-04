52cd6095da7a2e203195f145f30cf5adb39e7c87
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-04-04 10:25:26 -0500

    Configuracion Hibernate

--- UAMAsesorateMVN/src/main/java/com/chemasmas/uamasesoratemvn/models/HibernateUtil.java
+++ UAMAsesorateMVN/src/main/java/com/chemasmas/uamasesoratemvn/models/HibernateUtil.java
@@ -5,7 +5,8 @@
  */
 package com.chemasmas.uamasesoratemvn.models;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+
+import org.hibernate.cfg.Configuration;
 import org.hibernate.SessionFactory;
 
 /**
@@ -22,7 +23,7 @@ public class HibernateUtil {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            sessionFactory = new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
