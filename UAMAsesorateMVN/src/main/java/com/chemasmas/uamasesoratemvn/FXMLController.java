504954e3f406412c758be50e38b0099f11672561
jackycg <yaqi.cg@icloud.com>
2018-04-11 02:25:25 -0500

    agregue los componnetes de las pantallas detallesAsesoria y Contacto

--- UAMAsesorateMVN/src/main/java/com/chemasmas/uamasesoratemvn/FXMLController.java
+++ UAMAsesorateMVN/src/main/java/com/chemasmas/uamasesoratemvn/FXMLController.java
@@ -2,6 +2,8 @@ package com.chemasmas.uamasesoratemvn;
 
 import com.chemasmas.uamasesoratemvn.models.Divisiones;
 import com.chemasmas.uamasesoratemvn.models.HibernateUtil;
+import com.chemasmas.uamasesoratemvn.models.Profesores;
+import com.chemasmas.uamasesoratemvn.models.ProfesoresHasUeas;
 import com.chemasmas.uamasesoratemvn.models.Troncos;
 import com.chemasmas.uamasesoratemvn.models.Ueas;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -17,12 +19,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 public class FXMLController implements Initializable {
-    
+
     static Session session;
-    
+
     @FXML
     private Label label;
     @FXML
@@ -30,7 +34,7 @@ public class FXMLController implements Initializable {
     @FXML
     private Button buscar;
     @FXML
-    private TableView<?> resulatdosAsesoria;
+    private TableView<ProfesoresHasUeas> resulatdosAsesoria;
     @FXML
     private TableColumn<?, ?> nombreProfesor;
     @FXML
@@ -38,41 +42,48 @@ public class FXMLController implements Initializable {
     @FXML
     private TableColumn<?, ?> lugarAsesoria;
     @FXML
-    private TableColumn<?, ?> horraio;
-    @FXML
     private ComboBox<Divisiones> divisionCB;
     @FXML
     private ComboBox<Troncos> troncoCB;
     @FXML
     private ComboBox<Ueas> ueaCB;
-    
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        Dotenv dotenv = Dotenv.load();
-        label.setText(dotenv.get("msj"));   
-        
-        
-    }
-    
+    @FXML
+    private TableColumn<?, ?> horaioInicio;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         session = HibernateUtil.getSessionFactory().openSession();
-        
+
         List<Divisiones> lDiv = session.createCriteria(Divisiones.class).list();
         divisionCB.getItems().addAll(lDiv);
-        
+
         List<Troncos> lTro = session.createCriteria(Troncos.class).list();
         troncoCB.getItems().addAll(lTro);
-        
+
         List<Ueas> lUeas = session.createCriteria(Ueas.class).list();
         ueaCB.getItems().addAll(lUeas);
-    }    
+//
+//        Query query = session.createSQLQuery("select * from UEAs u where u.division = :division and u.tronco = :tronco")
+//        .addEntity(Profesores.class)
+//        .setParameter("division", division)
+//        .setParameter("tronco", tronco);
+//        List<Ueas> list = (List<Ueas>) query.list();
+
+    }
 
     @FXML
     private void buscarPorNombre(ActionEvent event) {
+        String nombUea = nombreUea.toString();
+        
     }
 
     @FXML
     private void agendar(ActionEvent event) {
+    }
+
+    @FXML
+    private void buscarPorDTU(ActionEvent event) {
+        
+       
     }
 }
